b50dfac07e6a5b1cfd5180034d0e55dd9054dbef
jpodlasiak23 <joanna.podlasiak@onet.pl>
2019-10-16 09:53:22 +0200

    Added new scene

--- NumberWizard/Assets/NumberWizard.cs
+++ NumberWizard/Assets/NumberWizard.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
     void Start()
     {
         print("Witaj u Liczbowego Czarodzieja");
         print("Pomyśl liczbę w głowie ale nie mów jej na głos");
-        int min = 1;
-        int max = 1000;
+        
         print("Największa liczba jaką możesz wybrać to:" + max);
         print("Najmniejsza liczba jaką możesz wybrać to:" + min);
         print("czy twoja liczba jest większa czy mniejsza od 500?");
         print("Aby odpowiedzieć na pytanie naciśnij: Górna strzałka = wyższa, Dolna strzałka = niższa, Naciśnij Enter = liczba którą wymyśliłeś");
-        if (Input.GetKeyDown( KeyCode.UpArrow)) ;
-        if (Input.GetKeyUp(KeyCode.UpArrow)) ;
-
-        print 
+      
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("użytkownik nacisnął dolną strzałkę");
+            min = guess;
+            guess = (min + max) / 2;
+            print("Czy twoja liczba jest mniejsza lub większa niż" + guess);
+        }
+            
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+            {
+            max = guess;
+            guess = (min + min) / 2; ;
+
+
+        print("czy twoja liczba jest większa lub mniejsza niż" +guess);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        print("Haha zgadłem twoją liczbę!");
+
+
+
     }
 }
